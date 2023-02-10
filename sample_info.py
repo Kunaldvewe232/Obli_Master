1036d67cab8cacbf5e69a28d39db80a736f876a2
Kunaldvewe232 <108283207+Kunaldvewe232@users.noreply.github.com>
2023-02-10 23:06:53 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,8 +1,8 @@
 # Bot information
-SESSION = 'Media_Search'
-USER_SESSION = 'BAFN200AByliJ5XijOxLL47FSpctPxIk8EHloEcxYfjgrexr6-SpbfrWqztbd0o6QB_LEocroL6yBxDnIQilaJK32lp7ayQzOe2v0swPdxjTphyW3YNHnGpT5hPi3LTJSRw5ohtfqkq7b1fq1rDgLGMf1j5PlbGCPM5a3Q0Xwc6DMBDPq7YJ0bV9I17w88HyKZOsMCyxpSK1KMgmL7-T1PL7kX9rWuPYKAdKdHUulB_GTaAk6rXK7XsJjQGGlDV68PpMuRfubCSUXGzSCccfGyEcAKimCIPW4Ww5skpLv6mu_y6kRGEaz2hJ49ylw-IIetaLME4dhaCt5ITwzvSoWiJJceclKAAAAAFNQAAoAA'
-API_ID = 21879629
-API_HASH = 'dcb6bfd6d51a8ff5f6aadb01b9fdd11b'
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 12345
+API_HASH = '0123456789abcdef0123456789abcdef'
 BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
 USERBOT_STRING_SESSION = ''
 
@@ -11,14 +11,13 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [5591007272, 'BATMAN', ]
-CHANNELS = [-1001519788177,]
-AUTH_USERS = [5591007272, 'BATMAN']
+ADMINS = [12345789, 'admin123', 98765432]
+CHANNELS = [-10012345678, -100987654321, 'channelusername']
+AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://soekksielks555:soekksielks555@cluster0.bi0jr0g.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
-COLLECTION_NAME = ''  # If you are using the same database, then use different collection name for each bot
-
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
