8cf40282f632529941fa520538f14a318a6b0f5a
Kunaldvewe232 <108283207+Kunaldvewe232@users.noreply.github.com>
2023-02-10 21:52:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('mongodb+srv://soekksielks555:soekksielks555@cluster0.bi0jr0g.mongodb.net/?retryWrites=true&w=majority', "")
-DATABASE_NAME = environ.get('DATABASE_URI = environ.get('cluster0', "ObliMaster")
+DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://soekksielks555:soekksielks555@cluster0.bi0jr0g.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('Dont Join', 'Telegram_files')
 
 # Others
